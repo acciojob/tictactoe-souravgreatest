aa190c70d6023c189bb37a26ad0253557934daa0
sourav jain <95373772+souravgreatest@users.noreply.github.com>
2025-03-05 18:38:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let a=player1,b=player2
+function players=(e)=>{
+	e.preventDefault();
+	a=document.getElementById('player-1').value;
+	b=document.getElementById('player-2').value;
+	document.getElementById('player-1').value="eojfj";
+	document.getElementById('player-2').value="difj";
+}
+
+
+window.onload = document.getElementById('Intro').onsubmit = players;
